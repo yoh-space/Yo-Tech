248c3e8fd793ade8b8c3c4d5282f01af50a35a71
Yoh-space <yohannesdamtie822@gmail.com>
2025-09-06 15:26:43 +0300

    routing to home after sign in and sign up 	modified:   src/app/signin/[[...rest]]/page.tsx 	modified:   src/app/signup/[[...rest]]/page.tsx

--- src/app/signin/[[...rest]]/page.tsx
+++ src/app/signin/[[...rest]]/page.tsx
@@ -4,7 +4,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function SignInPage() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <SignIn path="/signin" routing="path" />
+      <SignIn path="/signin" routing="path" afterSignInUrl="/"/>
     </div>
   );
 }
--- src/app/signup/[[...rest]]/page.tsx
+++ src/app/signup/[[...rest]]/page.tsx
@@ -4,7 +4,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function SignUpPage() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <SignUp path="/signup" routing="path" />
+      <SignUp path="/signup" routing="path" afterSignUpUrl="/"/>
     </div>
   );
 }
